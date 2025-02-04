50357468dee62c023f099470239f8c2da377a7db
WillWSL <meltonjw3@gmail.com>
2025-02-03 19:22:15 -0600

    reaploop created

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -3,7 +3,6 @@ package pokecache
 import (
 	"fmt"
 	"time"
-    "byte"
     "sync"
 	)
 
@@ -17,11 +16,29 @@ type cacheEntry struct {
 type Cache struct {
     m map[string]cacheEntry
     mu sync.Mutex
-    lifetime time.Duration 
+    lifetime time.Duration
+    func (c *Cache) reapLoop()
+}
+
+func reapLoop() {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+		
+	for range ticker.C { //clean cache entries
+		m.mu.Lock()		
+		for key, value := range cache.m {
+			if(value.Duration > m.lifetime) {
+				delete(cache.m,key)
+			}
+
+		}
+	}
+	m.mu.Unlock()
+
 }
 
 func NewCache(time time.Duration) *Cache {
-	m := make(map[string]cacheEntry
+	m := make(map[string]cacheEntry)
 
 	cache := &Cache{m: m, lifetime: time}
 
